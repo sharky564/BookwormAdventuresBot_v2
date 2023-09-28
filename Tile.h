6c7e6a42cb8dca3096e825eb629ef9792eff8531
sharky564 <sharkesa@gmail.com>
2023-09-29 01:21:35 +1000

    test commit

---
+++ Tile.h
@@ -0,0 +1,26 @@
+#ifndef TILE_H
+#define TILE_H
+
+#ifndef CONSTANTS_H
+#include "constants.h"
+#endif // CONSTANTS_H
+
+#include <functional>
+
+struct Tile {
+    char letter;
+    double pointScore;
+
+    Tile(char letter);
+
+    bool operator==(const Tile &other) const;
+};
+
+template <> struct std::hash<Tile> {
+    inline size_t operator()(const Tile &tile) const noexcept {
+        return std::hash<char>()(tile.letter);
+    };
+};
+
+
+#endif // TILE_H
